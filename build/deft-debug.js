971dc022a3b52e253126e9a0feea6cfdbeb7405b
Chris Westbrook <cwestbrook20@gmail.com>
2012-07-03 06:36:18 -0700

    Added Tracing DeftJS Controller Inheritance for debugging. Modified to only allow the first defined controller, rather than the last superclass.

--- build/deft-debug.js
+++ build/deft-debug.js
@@ -636,7 +636,7 @@ Ext.define('Deft.mvc.ViewController', {
 Ext.define('Deft.mixin.Controllable', {});
 
 Ext.Class.registerPreprocessor('controller', function (Class, data, hooks, callback) {
-  var controllerClass, originalConstructor, originalDestroy, parameters, self;
+  var controllerClass, originalConstructor, originalDestroy, parameters, self, controllerState, superclassController;
   if (arguments.length === 3) {
     parameters = Ext.toArray(arguments);
     hooks = parameters[1];
@@ -646,6 +646,13 @@ Ext.Class.registerPreprocessor('controller', function (Class, data, hooks, callb
     controllerClass = data.controller;
     delete data.controller;
     if (controllerClass != null) {
+
+      if (!this.__proto__.controllerState) {
+        this.__proto__.controllerState = {deftJS : {currentController : undefined, controllerChain : {attempted : [], processed : []}}};
+      }
+      controllerState = this.__proto__.controllerState;
+      controllerState.deftJS.controllerChain.attempted.push({id : Ext.id(undefined, 'attempted-controller'), class : controllerClass});
+
       if (!data.hasOwnProperty('constructor')) {
         data.constructor = function () {
           return this.callParent(arguments);
@@ -657,12 +664,22 @@ Ext.Class.registerPreprocessor('controller', function (Class, data, hooks, callb
         if (config == null) {
           config = {};
         }
-        try {
-          controller = Ext.create(controllerClass, config.controllerConfig || this.controllerConfig || {});
-        } catch (error) {
-          Deft.Logger.warn("Error initializing Controllable instance: an error occurred while creating an instance of the specified controller: '" + controllerClass + "'.");
-          throw error;
+        // TODO - This is too brute force, only accepts the first controller to be defined. That controller then must manage it's own 'Deft.mvc.ViewController' inheritance.
+        if (controllerState.deftJS.controllerChain.processed.length > 0) {
+          //superclassController = Class && Class.controller ? Class.superclass.controller : false;
+          console.warn("We should stop and do something here!");
+          controller = controllerState.deftJS.currentController;
+        } else {
+          controllerState.deftJS.controllerChain.processed.push(controllerClass);
+          try {
+            controller = Ext.create(controllerClass, config.controllerConfig || this.controllerConfig || {});
+            controllerState.deftJS.currentController = controller;
+          } catch (error) {
+            Deft.Logger.warn("Error initializing Controllable instance: an error occurred while creating an instance of the specified controller: '" + controllerClass + "'.");
+            throw error;
+          }
         }
+
         this.getController = function () {
           return controller;
         };
